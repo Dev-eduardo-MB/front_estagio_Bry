8157cdf0c1a3c856566b8ff71cb679a5d621d33a
Dev-eduardo-MB <eduardo.moreirabernardi@gmail.com>
2025-11-30 07:00:05 -0300

    Feat: Adiciona configuração completa de rotas substituindo o arquivo vazio por rotas de home, employees (lista, criação e edição) e companies (lista, criação, edição e info), todas carregadas via lazy loading e com fallback para home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Home → Redireciona para Employees
+  {   path: 'home',
+  loadComponent: () =>
+    import('./components/home/home')
+      .then(m => m.HomeComponent) },
+
+  // EMPLOYEES
+  {
+    path: 'employees',
+    loadComponent: () =>
+      import('./components/employees/employee-list/employee-list')
+        .then(m => m.EmployeeListComponent)
+  },
+  {
+    path: 'employees/new',
+    loadComponent: () =>
+      import('./components/employees/employee-form/employee-form')
+        .then(m => m.EmployeeForm)
+  },
+  {
+    path: 'employees/:id',
+    loadComponent: () =>
+      import('./components/employees/employee-edit/employee-edit')
+        .then(m => m.EmployeeEditComponent)
+  },
+
+  // COMPANIES
+  {
+    path: 'companies',
+    loadComponent: () =>
+      import('./components/companies/company-list/company-list')
+        .then(m => m.CompanyList)
+  },
+  {
+    path: 'companies/new',
+    loadComponent: () =>
+      import('./components/companies/company-form/company-form')
+        .then(m => m.CompanyForm)
+  },
+  {
+    path: 'companies/:id',
+    loadComponent: () =>
+      import('./components/companies/company-edit/company-edit')
+        .then(m => m.CompanyEditComponent)
+  },
+
+  {
+  path: 'companies/:id/info',
+  loadComponent: () =>
+    import('./components/companies/company-info/company-info')
+      .then(m => m.CompanyInfoComponent)
+},
+  
+
+  // Fallback
+  { path: '**', redirectTo: 'home' }
+];
